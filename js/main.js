c8b3aa172ce872963e5fd12606c92aead7ed9eed
Brian Francisco <ibrianfrancisco@gmail.com>
2016-12-09 16:54:30 -0800

    testing new branch

--- js/main.js
+++ js/main.js
@@ -28,52 +28,83 @@
 
 /* ---------- Functions ---------- */
 
+function showHowToPlay() {
+  $('.renderGame').fadeOut(1000);
+}
 
 
 
 
 /* ---------- Initial page ---------- */
-$howToPlay = $('button')
+
+$('.renderGame').on('click', showHowToPlay)
 
 
-$howToPlay.on('click', showHowToPlay)
 
-function showHowToPlay() {
-  $howToPlay.fadeOut(2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*----------- Deck of Cards ---------*/
+
+var suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+
+var vals = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+
+var Card = function(suit, val) {
+  this.suit = suit
+  this.val = val
+  this.stateValue = function(){
+    return 'The ' + this.val + ' of ' + this.suit + '.'
+  }
 }
 
+var deck = []
 
+for(var i = 0; i < vals.length; i++){
+  for(var j = 0; j < suits.length; j++){
+    var card = new Card(suits[j], vals[i])
+    deck.push(card)
+  }
+}
 
-
-
-
-
-
-
-
-
+// document.getElementById('showDeck').addEventListener('click', function() {
+//   console.log(deck)
+//   console.log(deck.length)
+// });
 
 
 /*------------ Form Input for betting -----------*/
-function FilterInput(event) {
-    var keyCode = ('which' in event) ? event.which : event.keyCode;
+// function FilterInput(event) {
+//     var keyCode = ('which' in event) ? event.which : event.keyCode;
 
-    isNotWanted = (keyCode == 69 || keyCode == 101);
-    return !isNotWanted;
-}
-function handlePaste (e) {
-    var clipboardData, pastedData;
+//     isNotWanted = (keyCode == 69 || keyCode == 101);
+//     return !isNotWanted;
+// }
+// function handlePaste (e) {
+//     var clipboardData, pastedData;
 
-    // Get pasted data via clipboard API
-    clipboardData = e.clipboardData || window.clipboardData;
-    pastedData = clipboardData.getData('Text').toUpperCase();
+//     // Get pasted data via clipboard API
+//     clipboardData = e.clipboardData || window.clipboardData;
+//     pastedData = clipboardData.getData('Text').toUpperCase();
 
-    if(pastedData.indexOf('E')>-1) {
-        //alert('found an E');
-        e.stopPropagation();
-        e.preventDefault();
-    }
-}
+//     if(pastedData.indexOf('E')>-1) {
+//         //alert('found an E');
+//         e.stopPropagation();
+//         e.preventDefault();
+//     }
+// }
 
 
 // Test this for multiple aces that come up
