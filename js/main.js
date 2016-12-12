01df8ed578f462c79a6d842edc3f9c60a50a95b5
Brian Francisco <ibrianfrancisco@gmail.com>
2016-12-12 10:48:51 -0800

    added functionality to Hit Button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 /* ---------- Variables ---------- */
-var playerFunds;
+var playerFunds = 500;
 var amountBet;
-
-
-
-
-
-
-
-
 
 
 /* ---------- Event Listeners ---------- */
@@ -34,10 +26,9 @@ $("#letsPlay").click(function() {
 })
 
 
-
 // initial draw 2 cards for playerHand. ***Need to push into playerhand empty []***
   // Try to use jQuery .each instead of for loop. But this is good for now
-var drawACard = $(".submitBet").click(function() {
+$("#submitBet").click(function() {
   for(var i=0;i<2;i++) {
     var x = Math.floor(Math.random() * 52);
     playerHand.push(deck[x]);
@@ -49,9 +40,15 @@ var drawACard = $(".submitBet").click(function() {
 /* ---------- Functions ---------- */
 
 
-
-
-
+// Hit button
+$("#hitButton").click(function() {
+  var x = Math.floor(Math.random() * 52);
+  playerHand.push(deck[x]);
+  $('.playercards').each(function(idx) {
+    $(this).addClass(cardClasses[x]);
+  });
+  console.log(deck[x]);
+});
 
 
 
@@ -62,20 +59,41 @@ var drawACard = $(".submitBet").click(function() {
 
 /* ---------- Game page ---------- */
 
-var count = 0;
+var amountBet = 0;
 
 $("#bet5").click(function() {
-  count += 5;
-  $("#counter").html(count);
+  amountBet += 5;
+  $("#counter").html(amountBet);
+});
+$("#bet10").click(function() {
+  amountBet += 10;
+  $("#counter").html(amountBet);
+});
+$("#bet20").click(function() {
+  amountBet += 20;
+  $("#counter").html(amountBet);
+});
+$("#bet50").click(function() {
+  amountBet += 50;
+  $("#counter").html(amountBet);
+});
+$("#bet100").click(function() {
+  amountBet += 100;
+  $("#counter").html(amountBet);
+});
+$(".erase").click(function() {
+  amountBet = 0;
+  $("#counter").html(amountBet);
 });
 
 
-
-
-
-
-
-
+// function adjustBet() {
+//   if(player > dealer) {
+//     amountBet *= 2
+//     playerFunds += amountBet
+//     amountBet = 0
+//   }
+// } else if (player < dealer)
 
 
 
@@ -84,6 +102,8 @@ $("#bet5").click(function() {
 
 
 /*----------- Deck of Cards ---------*/
+
+var cardClasses = ['sA','hA','cA','dA','s02','h02','c02','d02','s03','h03','c03','d03','s04','h04','c04','d04','s05','h05','c05','d05','s06','h06','c06','d06','s07','h07','c07','d07','s08','h08','c08','d08','s09','h09','c09','d09','s10','h10','c10','d10','sJ','hJ','cJ','dJ','sQ','hQ','cQ','dQ','sQ','hK','cK','dK']
 
 var suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 var vals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -104,10 +124,10 @@ for(var i = 0; i < vals.length; i++){
   }
 }
 
-// document.getElementById("showDeck").addEventListener("click", function() {
-//   console.log(deck)
-//   console.log(deck.length)
-// });
+document.getElementById("standButton").addEventListener("click", function() {
+  console.log(deck)
+  console.log(deck.length)
+});
 
 
 // Test this for multiple aces that come up
