25116e4a73f376fe4075b336c13a771c5f0a7b89
Brian Francisco <ibrianfrancisco@gmail.com>
2016-12-13 10:47:38 -0800

    bet button prevents overbetting

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
 /* ---------- Variables ---------- */
 var playerFunds = 500;
-var amountBet = 0;
-$("#player-fund").html(playerFunds);
+
 
 /* ---------- Event Listeners ---------- */
 // fadeout Initial page and fadein my Game Page
-$("#lets-play-button").click(function() {
-  $(".main-page").fadeOut(800, function() {
-    $("#game-page").fadeIn(800);
+$('#lets-play-button').click(function() {
+  $('.main-page').fadeOut(800, function() {
+    $('#game-page').fadeIn(800);
   });
 });
 
 // fadeout Initial page and fadein How To Play
-$("#how-to-button").click(function() {
-  $(".main-page").fadeOut(800, function() {
-    $("#how-to-play").fadeIn(800);
+$('#how-to-button').click(function() {
+  $('.main-page').fadeOut(800, function() {
+    $('#how-to-play').fadeIn(800);
   });
 });
 
 // fadeout How To Play and fadein Game Page
-$("#lets-play").click(function() {
-  $("#how-to-play").fadeOut(800, function() {
-    $("#game-page").fadeIn(800);
+$('#lets-play').click(function() {
+  $('#how-to-play').fadeOut(800, function() {
+    $('#game-page').fadeIn(800);
   });
 });
 
 
 // BET BUTTON: initial draw 2 cards for playerHand.
-$("#submit-bet").click(function() {
+$('#submit-bet').click(function() {
+  if (amountBet > playerFunds || amountBet === 0) {
+    return alert("Check Funds");
+  }
   for(var i=0;i<2;i++) {
     var x = Math.floor(Math.random() * 52);
     playerHand.push(deck[x]);
     var el = document.getElementsByClassName('player-cards')[i];
     el.classList.add(cardClasses[x]);
     playerScoreTotal();
-    $("#player-value").html(playerScore);
+    $('#player-value').html(playerScore);
   }
   for(var i=0; i<1;i++) {
     var x = Math.floor(Math.random() * 52);
@@ -42,30 +44,33 @@ $("#submit-bet").click(function() {
     var el = document.getElementsByClassName('dealer-cards')[i];
     el.classList.add(cardClasses[x]);
     dealerScoreTotal();
-    $("#dealer-value").html(dealerScore);
+    $('#dealer-value').html(dealerScore);
   }
   if (playerScore === 21) {
-    alert("Player got BlackJack!")
+    alert('Player got BlackJack!');
+    amountBet *= 2;
+    playerFunds += amountBet;
+    return $('#player-fund').html(playerFunds);
   }
 });
 
 
 // HIT BUTTON: adding one card per click
-$("#hit-button").click(function() {
+$('#hit-button').click(function() {
   var x = Math.floor(Math.random() * 52);
   playerHand.push(deck[x]);
   for (var i = 0; i < playerHand.length; i++)
     var el = document.getElementsByClassName('player-cards')[i];
     el.classList.add(cardClasses[x]);
     playerScoreTotal();
-    $("#player-value").html(playerScore);
+    $('#player-value').html(playerScore);
   if (playerScore > 21) {
-    alert("Player Bust");
+    alert('Player Bust');
   }
 });
 
 // STAND BUTTON: do everything
-$("#stand-button").click(function() {
+$('#stand-button').click(function() {
   while (dealerScore < 17) {
     var x = Math.floor(Math.random() * 52);
     dealerHand.push(deck[x]);
@@ -74,51 +79,60 @@ $("#stand-button").click(function() {
     el.classList.add(cardClasses[x]);
     dealerScoreTotal();
   }
-  $("#dealer-value").html(dealerScore);
+  $('#dealer-value').html(dealerScore);
   if (dealerScore > 21) {
-    alert("Player WINS");
+    alert('Player WINS');
   } else if (playerScore > dealerScore) {
-    alert("Player WINS");
+    alert('Player WINS');
   } else if (playerScore < dealerScore) {
-    alert("Dealer WINS");
+    alert('Dealer WINS');
   } else if (playerScore === dealerScore) {
-    alert("PUSH");
+    alert('PUSH');
   }
 });
 
 
 // BETTING CHIPS: updates amountBet.
-$("#bet5").click(function() {
+$('#bet5').click(function() {
   amountBet += 5;
-  $("#counter").html(amountBet);
+  $('#counter').html(amountBet);
 });
-$("#bet10").click(function() {
+$('#bet10').click(function() {
   amountBet += 10;
-  $("#counter").html(amountBet);
+  $('#counter').html(amountBet);
 });
-$("#bet20").click(function() {
+$('#bet20').click(function() {
   amountBet += 20;
-  $("#counter").html(amountBet);
+  $('#counter').html(amountBet);
 });
-$("#bet50").click(function() {
+$('#bet50').click(function() {
   amountBet += 50;
-  $("#counter").html(amountBet);
+  $('#counter').html(amountBet);
 });
-$("#bet100").click(function() {
+$('#bet100').click(function() {
   amountBet += 100;
-  $("#counter").html(amountBet);
+  $('#counter').html(amountBet);
 });
-$(".erase").click(function() {
+$('.erase').click(function() {
   amountBet = 0;
-  $("#counter").html(amountBet);
+  $('#counter').html(amountBet);
 });
 
-/* ---------- Functions ---------- */
 
 // function to add amountBet to bet button
 // function saveBet() {
+//   if (playerScore)
 // }
 
+var amountBet = 0;
+$('#player-fund').html(playerFunds);
+
+
+/* ---------- Functions ---------- */
+function erase() {
+  amountBet = 0;
+  $('#counter').html(amountBet);
+}
 
 function playerScoreTotal() {
   playerScore = 0;
@@ -169,7 +183,7 @@ function dealerScoreTotal() {
 
 var cardClasses = ['sA','hA','cA','dA','s02','h02','c02','d02','s03','h03','c03','d03','s04','h04','c04','d04','s05','h05','c05','d05','s06','h06','c06','d06','s07','h07','c07','d07','s08','h08','c08','d08','s09','h09','c09','d09','s10','h10','c10','d10','sJ','hJ','cJ','dJ','sQ','hQ','cQ','dQ','sQ','hK','cK','dK'];
 
-var suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+var suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
 var vals = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 var deck = [];
 var playerHand = [];
@@ -188,10 +202,13 @@ for(var i = 0; i < vals.length; i++){
   }
 }
 
-$("#deal-again").click(function() {
+{$('#deal-again').click(function() {
   playerHand = [];
   dealerHand = [];
-  $(".player-cards").attr("class", "").addClass("card player-cards");
-  $(".dealer-cards").attr("class", "").addClass("card dealer-cards");
-  $(".display-values").html("");
-});
+  $('.player-cards').attr('class', '').addClass('card player-cards');
+  $('.dealer-cards').attr('class', '').addClass('card dealer-cards');
+  $('.display-values').html('');
+  $('#counter').html(0);
+  erase();
+  });
+};
