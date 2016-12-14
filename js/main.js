25084824cdc5295c2d30d82dcf04a72fdc8ae69d
Brian Francisco <ibrianfrancisco@gmail.com>
2016-12-13 17:47:44 -0800

    Need to replenish deck

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* ---------- Variables ---------- */
 var playerFunds = 500;
 var amountBet = 0;
+var playerScore;
 $('#player-fund').html(playerFunds);
 
 /* ---------- Event Listeners ---------- */
@@ -25,14 +26,17 @@ $('#lets-play').click(function() {
   });
 });
 
-
 // BET BUTTON: initial draw 2 cards for playerHand.
 $('#submit-bet').click(function() {
   if (amountBet > playerFunds || amountBet === 0) {
     return alert("Check Funds");
   }
+  if (playerScore > 0) {
+    return;
+  }
   for(var i=0;i<2;i++) {
     var x = Math.floor(Math.random() * 52);
+    deck.splice(x, 1);
     playerHand.push(deck[x]);
     var el = document.getElementsByClassName('player-cards')[i];
     el.classList.remove('back-blue');
@@ -42,6 +46,7 @@ $('#submit-bet').click(function() {
   }
   for(var i=0; i<1;i++) {
     var x = Math.floor(Math.random() * 52);
+    deck.splice(x, 1);
     dealerHand.push(deck[x]);
     var el = document.getElementsByClassName('dealer-cards')[i];
     el.classList.remove('back-blue');
@@ -51,16 +56,16 @@ $('#submit-bet').click(function() {
   }
   if (playerScore === 21) {
     alert('Player got BlackJack!');
-    amountBet *= 2.5;
+    amountBet *= 1.5;
     playerFunds += amountBet;
     return $('#player-fund').html(playerFunds);
   }
 });
 
-
 // HIT BUTTON: adding one card per click
 $('#hit-button').click(function() {
   var x = Math.floor(Math.random() * 52);
+  deck.splice(x, 1);
   playerHand.push(deck[x]);
   for (var i = 0; i < playerHand.length; i++)
     var el = document.getElementsByClassName('player-cards')[i];
@@ -79,6 +84,7 @@ $('#hit-button').click(function() {
 $('#stand-button').click(function() {
   while (dealerScore < 17) {
     var x = Math.floor(Math.random() * 52);
+    deck.splice(x, 1);
     dealerHand.push(deck[x]);
     for(var i=1; i<dealerHand.length; i++)
     var el = document.getElementsByClassName('dealer-cards')[i];
@@ -98,25 +104,24 @@ $('#stand-button').click(function() {
   }
 });
 
-
 // BETTING CHIPS: updates amountBet.
-$('#bet5').click(function() {
+$('#bet10').click(function() {
   amountBet += 10;
   $('#counter').html(amountBet);
 });
-$('#bet10').click(function() {
+$('#bet20').click(function() {
   amountBet += 20;
   $('#counter').html(amountBet);
 });
-$('#bet20').click(function() {
+$('#bet50').click(function() {
   amountBet += 50;
   $('#counter').html(amountBet);
 });
-$('#bet50').click(function() {
+$('#bet100').click(function() {
   amountBet += 100;
   $('#counter').html(amountBet);
 });
-$('#bet100').click(function() {
+$('#bet500').click(function() {
   amountBet += 500;
   $('#counter').html(amountBet);
 });
@@ -124,13 +129,6 @@ $('#erase').click(function() {
   amountBet = 0;
   $('#counter').html(amountBet);
 });
-
-
-// function to add amountBet to bet button
-// function saveBet() {
-//   if (playerScore)
-// }
-
 
 /* ---------- Functions ---------- */
 function playerScoreTotal() {
@@ -178,23 +176,9 @@ function dealerWins() {
   return $('#player-fund').html(playerFunds);
 }
 
-/* ---------- Game page ---------- */
-
-// BETTING WIN/LOSS MATH:
-//
-// function adjustBet() {
-//   if(player > dealer) {
-//     amountBet *= 2
-//     playerFunds += amountBet
-//     amountBet = 0
-//   }
-// } else if (player < dealer)
-
-
 /*----------- Deck of Cards ---------*/
 
 var cardClasses = ['sA','hA','cA','dA','s02','h02','c02','d02','s03','h03','c03','d03','s04','h04','c04','d04','s05','h05','c05','d05','s06','h06','c06','d06','s07','h07','c07','d07','s08','h08','c08','d08','s09','h09','c09','d09','s10','h10','c10','d10','sJ','hJ','cJ','dJ','sQ','hQ','cQ','dQ','sQ','hK','cK','dK'];
-
 var suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
 var vals = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 var deck = [];
@@ -214,7 +198,7 @@ for(var i = 0; i < vals.length; i++){
   }
 }
 
-{$('#deal-again').click(function() {
+$('#deal-again').click(function() {
   playerHand = [];
   dealerHand = [];
   $('.player-cards').attr('class', '').addClass('card player-cards back-blue');
@@ -222,6 +206,7 @@ for(var i = 0; i < vals.length; i++){
   $('.display-values').html('');
   $('#counter').html(0);
   amountBet = 0;
+  playerScore = 0;
+  dealerScore = undefined;
   $('#counter').html(amountBet);
   });
-};
